35ac72cd3ef79cfd51621ff9cf18f72d5eff539e
Mrakdatkom <markryanzipagan3@gmail.com>
2026-09-17 15:16:46 +0800

    setup: server.js file

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { config } from "./config/index.js";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middleware/errorMiddleware.js";
+import { connectDb } from "./config/db.js";
+import cors from "cors";
+
+const app = express();
+const PORT = config.port;
+
+app.use(helmet());
+
+app.use(cors({
+    origin: config.frontendUrl,
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Ratelimiter
+
+// Routes
+
+app.use(errorMiddleware);
+
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log(`DB connected to ${PORT}`);
+    });
+});
