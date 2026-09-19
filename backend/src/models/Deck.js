a0cae407809b647023c2a207db682bd4875380d5
Mrakdatkom <markryanzipagan3@gmail.com>
2026-09-19 22:46:45 +0800

    fix: typo userId reference

--- backend/src/models/Deck.js
+++ backend/src/models/Deck.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const deckSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: user,
+        ref: "User",
         required: true,
     },
     title: {
